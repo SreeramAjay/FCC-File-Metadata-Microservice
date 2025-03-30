a50ee4de1a6a02e03d214fbceb28555762a400d3
Ajay Sreeram <sriramajay9676@gmail.com>
2025-03-31 00:09:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,14 +13,17 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-app.post("/api/fileanalyse", upload.single('upfile'), (req, res) => {
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ error: "No file uploaded" });
+  }
+
   res.json({
     name: req.file.originalname,
     type: req.file.mimetype,
-    size: req.file.size
-  })
+    size: req.file.size,
+  });
 });
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
